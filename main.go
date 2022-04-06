677cf808e48c2a500a3fa3b4b406e429e64f1cb6
Mrsylar <guowensylar@aliyun.com>
2022-04-06 16:37:48 +0800

    testGitDesktopBranch
    testGitDesktopBranch

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 var a = 100
+var b = "sylar"
 
 func main() {
 
 	fmt.Printf("当前 value 是：%d", a)
+	fmt.Printf("Hello,my master：%v", b)
+
 }
 
 // some tools needed to be installed
