4b96e1e0561abe511541c7f072b6007915708d12
Mrsylar <guowensylar@aliyun.com>
2022-04-06 15:28:35 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("hello world!")
+}
+
+// some tools needed to be installed
+
+//  go-outline
+//   gotests
+//   gomodifytags
+//   impl
+//   goplay
+//   dlv
+//   staticcheck
+// gopls
